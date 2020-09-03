ca0ac58a843fc3eff32887ab8eadd7727cbb7c0d
Thiago Evaristo <thiagoers@outlook.com>
2020-09-03 18:02:39 -0300

    Sequelize configured.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const db = require('./models');
+
 const authController = require('./controllers/auth');
 
 const app = express();
@@ -8,8 +10,10 @@ app.use('/auth', authController);
 
 app.get('/', (req,res) => {
   return res.json('Api running...');
-})
+});
 
-app.listen(3001, () => {
-  console.log('Listening on port 3001');
+db.sequelize.sync().then(()=>{
+  app.listen(3001, () => {
+    console.log('Listening on port 3001');
+  });  
 });
